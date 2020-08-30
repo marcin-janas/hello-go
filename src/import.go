9518d2c1415db5e35936bd2bfe080a0b60802619
Marcin Janas <marcin.janas@airhelp.com>
2020-08-30 22:26:55 +0200

    feat: add slide about import
    Signed-off-by: Marcin Janas <marcin.janas@airhelp.com>

---
+++ src/import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello, Go!") // Println function from the fmt package
+	log.Println("Hello, Go!") // Println function from the log package
+}
