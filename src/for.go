8fb350528593482d3bad477ae39401e1f6e69dbb
Marcin Janas <marcin.janas@airhelp.com>
2020-08-30 22:50:13 +0200

    feat: add slide about for statement
    Signed-off-by: Marcin Janas <marcin.janas@airhelp.com>

---
+++ src/for.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// START OMIT
+	for {
+	}
+
+	s := []int{1, 2, 3, 4, 5}
+	for n, v := range s {
+		println(n, v)
+	}
+
+	for i := 0; i <= 9; i++ {
+		println(i)
+	}
+	// END OMIT
+}
